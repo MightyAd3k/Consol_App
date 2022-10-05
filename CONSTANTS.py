fb348dee4dd5c4177d612c50d14394aa6c9065ae
Adrian Drewniak <adriandrewniak19@gmail.com>
2022-10-05 23:05:20 +0200

    save new user to database

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -16,3 +16,6 @@ MESSAGES_TABLE = """CREATE TABLE messages(
     text varchar(255),
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )"""
+
+ALPHABET = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
--- models.py
+++ models.py
@@ -1,3 +1,7 @@
+import psycopg2
+from CONSTANTS import HOST, PASSWORD, PORT, USER
+from hash_password import hash_password
+
 class User:
     def __init__(self, username="", password="", salt=""):
         self._id = -1
@@ -12,4 +16,35 @@ class User:
     def hashed_password(self):
         return self._hashed_password
 
+    def set_password(self, password, salt=""):
+        self._hashed_password = hash_password(password, salt)
+
+    @hashed_password.setter
+    def hashed_password(self, password):
+        self.set_password(password)
+
+    def save_to_db(self, cursor):
+        if self._id == -1:
+            sql = "INSERT INTO users (username, hashed_password) VALUES(%s, %s) RETURNING id"
+            values = (self.username, self.hashed_password)
+            cursor.execute(sql, values)
+            self._id = cursor.fetchone()[0]
+            return True
+        return False
     
+
+
+
+# connection = psycopg2.connect(
+#             host=HOST, 
+#             user=USER, 
+#             password=PASSWORD,
+#             port = PORT,
+#             database = "console_app_db"
+#         )
+
+
+# connection.autocommit = True
+# cursor = connection.cursor()
+# new_user = User('Adek', 'password123')
+# new_user.save_to_db(cursor)
