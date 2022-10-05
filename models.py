422ba7344a3a4fecc9d3f76d24f6627de3339499
Adrian Drewniak <adriandrewniak19@gmail.com>
2022-10-05 22:39:00 +0200

    create class User

---
+++ models.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, username="", password="", salt=""):
+        self._id = -1
+        self.username = username
+        self._hashed_password = hash_password(password, salt)
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def hashed_password(self):
+        return self._hashed_password
+
+    
